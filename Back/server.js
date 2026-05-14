11e60504b27845f3bb5c5ff9a032e0cce860d0b0
Jean-Emmanuel Gallo <gallojean.emmanuel@gmail.com>
2026-05-14 23:48:38 +0200

    fix CORS 1

--- Back/server.js
+++ Back/server.js
@@ -14,13 +14,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
-app.options(/.*/, cors());app.use(express.json());
+app.use(express.json());
 
 app.use('/auth', authRoutes);
 
