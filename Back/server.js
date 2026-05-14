d171af42f966e4ffbb5e84b49dc89d1336af9c51
Jean-Emmanuel Gallo <gallojean.emmanuel@gmail.com>
2026-05-14 23:15:59 +0200

    fix Render port problem

--- Back/server.js
+++ Back/server.js
@@ -32,10 +32,9 @@ async function startServer() {
     console.log('MongoDB est connecté !');
 
 
-    app.listen(PORT, () => {
-      console.log(`Serveur lancé sur le port ${PORT}`);
-    });
-
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
+});
   } catch (err) {
 
     console.error('Erreur MongoDB :', err);
